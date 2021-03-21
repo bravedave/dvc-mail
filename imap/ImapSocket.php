ef5627eb839cc2dd4a2f0df8e1eb4afcd4354d55
Dave Bray <david@brayworth.com.au>
2021-03-21 15:13:11 +1000

    adding overview capability

--- imap/ImapSocket.php
+++ imap/ImapSocket.php
@@ -84,8 +84,8 @@ class ImapSocket {
 
   }
 
-  public function get_flags(int $uid): array {
-    $result = $this->send("FETCH $uid (FLAGS)");
+  public function get_flags(int $id): array {
+    $result = $this->send("FETCH $id (FLAGS)");
     preg_match_all("|\\* \\d+ FETCH \\(FLAGS \\((.*)\\)\\)|", $result[0], $matches);
     if (isset($matches[1][0])) {
       return explode(' ', $matches[1][0]);
@@ -95,6 +95,77 @@ class ImapSocket {
       return [];
 
     }
+
+  }
+
+  public function get_overview(int $id) {
+
+    $result = $this->send("FETCH $id (UID FLAGS BODY.PEEK[HEADER.FIELDS (Date To CC From Subject Message-ID)])");
+
+    $uid = preg_replace( '@(^.*\(UID | FLAGS .*$)@', '', $result[0]);
+    // \sys::logger( sprintf('<%s> %s', $uid, __METHOD__));
+
+    $headers = [$result[1]];
+    while ( $str = trim( fgets($this->socket))) {
+      if ( FALSE !== \strstr( $str, 'OK Fetch completed')) break;
+      if ( ')' == $str) continue; // end is nigh
+      $headers[] = $str;
+
+    }
+
+    $_headers = [
+      'id' => $id,
+      'uid' => $uid,
+      'From' => '',
+      'Date' => '',
+      'Message-ID' => '',
+      'Subject' => '',
+      'To' => '',
+      'CC' => '',
+      'flags' => []
+
+    ];
+
+    foreach ($headers as $header) {
+      if ( $str = $header) {
+
+        if ( preg_match('@^From:@', $str)) {
+          $_headers['From'] = trim( str_replace( 'From:', '', $str));
+
+        }
+        elseif ( preg_match('@^Date:@', $str)) {
+          $_headers['Date'] = trim( str_replace( 'Date:', '', $str));
+
+        }
+        elseif ( preg_match('@^Message-ID:@', $str)) {
+          $_headers['Message-ID'] = trim( str_replace( 'Message-ID:', '', $str));
+
+        }
+        elseif ( preg_match('@^Subject:@', $str)) {
+          $_headers['Subject'] = trim( str_replace( 'Subject:', '', $str));
+
+        }
+        elseif ( preg_match('@^To:@', $str)) {
+          $_headers['To'] = trim( str_replace( 'To:', '', $str));
+
+        }
+        elseif ( preg_match('@^CC:@', $str)) {
+          $_headers['CC'] = trim( str_replace( 'CC:', '', $str));
+
+        }
+        else {
+          \sys::logger( sprintf('<%s> UNKNOWN %s', $str, __METHOD__));
+
+        }
+
+      }
+
+    }
+
+    $flags = preg_replace( '@(^.*FLAGS \(|\) BODY\[.*$)@', '', $result[0]);
+    if (isset($flags)) $_headers['flags'] = explode(' ', $flags);
+
+    return $_headers;
 
   }
 
@@ -115,12 +186,21 @@ class ImapSocket {
   private function gets() {
     $result = [];
 
-    while (substr($str = fgets($this->socket), 0, 1) == '*') {
-      $result[] = substr($str, 0, -2);
+    $start = false;
+    while ( $str = fgets($this->socket)) {
+      if (substr($str, 0, 1) == '*') {
+        $result[] = substr($str, 0, -2);
+        $start = true;
+
+      }
+      elseif ( $start) {
+        break;
+
+      }
 
     }
-    $result[] = substr($str, 0, -2);
 
+    $result[] = substr($str, 0, -2);
     return $result;
 
   }
